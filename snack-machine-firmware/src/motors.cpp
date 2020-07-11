0fda910ed124707ab448d5b56fa9619aef52cbf1
Justin Nesselrotte <admin@jnesselr.org>
2020-07-10 20:36:56 -0600

    Adding code to control motors

--- snack-machine-firmware/src/motors.cpp
+++ snack-machine-firmware/src/motors.cpp
@@ -3,18 +3,27 @@
 #include "Arduino.h"
 #include "motors.h"
 
-static const int COL_1 = 37;
-static const int COL_2 = 36;
-static const int COL_3 = 35;
-static const int COL_4 = 34;
-static const int COL_5 = 33;
-static const int COL_6 = 32;
-static const int COL_7 = 31;
-static const int COL_8 = 30;
+const int Motors::ROWS[] = {
+  Motors::ROW_1,
+  Motors::ROW_2,
+  Motors::ROW_3,
+  Motors::ROW_4,
+  Motors::ROW_5,
+  Motors::ROW_6,
+  Motors::ROW_7,
+  Motors::ROW_8
+};
 
-const int ROWS[] = {49, 48, 47, 46, 45, 44, 43, 42};
-
-const int COLS[] = {37, 36, 35, 34, 33, 32, 31, 30};
+const int Motors::COLS[] = {
+  Motors::COL_1,
+  Motors::COL_2,
+  Motors::COL_3,
+  Motors::COL_4,
+  Motors::COL_5,
+  Motors::COL_6,
+  Motors::COL_7,
+  Motors::COL_8
+};
 
 void Motors::setup() {
   pinMode(ROWS[0], OUTPUT);
@@ -33,15 +42,74 @@ void Motors::setup() {
   pinMode(COLS[5], OUTPUT);
   pinMode(COLS[6], OUTPUT);
   pinMode(COLS[7], OUTPUT);
-  pinMode(Motors::MOTORS_ENABLE, OUTPUT);
-  digitalWrite(MOTORS_ENABLE, HIGH);
+  Motors::off();
+
+  pinMode(MOTORS_ENABLE, OUTPUT);
+  digitalWrite(MOTORS_ENABLE, LOW);
+
+  pinMode(MOTORS_SENSE, INPUT);
+}
+
+bool Motors::exists(int row, int col) {
+  if(!valid(row, col)) {
+    return false;
+  }
+
+  bool result = false;
+
+  digitalWrite(MOTORS_ENABLE, LOW);
+  digitalWrite(ROWS[row], HIGH);
+  digitalWrite(COLS[col], HIGH);
+  delay(1000);
+  int value = analogRead(MOTORS_SENSE);
+  if(value < 100) {
+    result = true;
+  }
+  off();
+
+  return result;
 }
 
 void Motors::vend(int row, int col) {
+  if(!Motors::valid(row, col)) {
+    return;
+  }
+  
+  digitalWrite(MOTORS_ENABLE, HIGH);
   digitalWrite(ROWS[row], HIGH);
   digitalWrite(COLS[col], HIGH);
-  delay(500);
+  delay(400);
   digitalWrite(COLS[col], LOW);
+  delay(3000);
+  off();
+}
+
+void Motors::off() {
+  digitalWrite(MOTORS_ENABLE, LOW);
+  digitalWrite(ROWS[0], LOW);
+  digitalWrite(ROWS[1], LOW);
+  digitalWrite(ROWS[2], LOW);
+  digitalWrite(ROWS[3], LOW);
+  digitalWrite(ROWS[4], LOW);
+  digitalWrite(ROWS[5], LOW);
+  digitalWrite(ROWS[6], LOW);
+  digitalWrite(ROWS[7], LOW);
+  digitalWrite(COLS[0], LOW);
+  digitalWrite(COLS[1], LOW);
+  digitalWrite(COLS[2], LOW);
+  digitalWrite(COLS[3], LOW);
+  digitalWrite(COLS[4], LOW);
+  digitalWrite(COLS[5], LOW);
+  digitalWrite(COLS[6], LOW);
+  digitalWrite(COLS[7], LOW);
+}
+
+bool Motors::valid(int row, int col) {
+  if(row < 1) return false;
+  if(row > 8) return false;
+  if(col < 1) return false;
+  if(col > 8) return false;
+  return true;
 }
 
 #endif
--- snack-machine-firmware/src/motors.h
+++ snack-machine-firmware/src/motors.h
@@ -3,8 +3,29 @@ class Motors {
     static void setup();
 
     static void vend(int row, int col);
+    static void off();
   private:
+    static bool valid(int row, int col);
 
     static const int MOTORS_ENABLE = 20;
-    static const int MOTORS_FAULT = 21;
+    static const int MOTORS_SENSE = PIN_A12;
+    static const int ROW_1 = 49;
+    static const int ROW_2 = 48;
+    static const int ROW_3 = 47;
+    static const int ROW_4 = 46;
+    static const int ROW_5 = 45;
+    static const int ROW_6 = 44;
+    static const int ROW_7 = 43;
+    static const int ROW_8 = 42;
+    static const int COL_1 = 37;
+    static const int COL_2 = 36;
+    static const int COL_3 = 35;
+    static const int COL_4 = 34;
+    static const int COL_5 = 33;
+    static const int COL_6 = 32;
+    static const int COL_7 = 31;
+    static const int COL_8 = 30;
+
+    static const int ROWS[9];
+    static const int COLS[9];
 };
